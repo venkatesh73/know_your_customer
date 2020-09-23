767c200f0ebb8379973c21b330bd05c7011ba556
Venkatesh Shanmugham <hackersleepyhead@gmail.com>
2020-09-23 19:29:03 +0530

    Added data seeder to pre-populate data for topics

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -1,11 +1,22 @@
-# Script for populating the database. You can run it as:
-#
-#     mix run priv/repo/seeds.exs
-#
-# Inside the script, you can read and write to any of your
-# repositories directly:
-#
-#     KYC.Repo.insert!(%KYC.SomeSchema{})
-#
-# We recommend using the bang functions (`insert!`, `update!`
-# and so on) as they will fail if something goes wrong.
+defmodule KYC.DataSeeder do
+  alias KYC.Repo
+  alias KYC.Schema.Topics
+
+
+  @topics_of_interest "Photography,Space,Sports,Cars,Rock Music,Hip Hop,Hiking,Climbing,Cycling,Retro Dance,Bikes,Moto Cross,PS4,PubG,IPhone,Books,Fiction Story,Sic-Fi Movies,Tom-Cruise,Batman,Marvel Movies,Avengers,Iron-Man"
+
+  def seed() do
+    Repo.delete_all(Topics)
+
+    @topics_of_interest
+    |> String.split(",", trim: true)
+    |> Enum.map( 
+      &Repo.insert!(%Topics{
+        name: &1,
+        descp: "Some description about the topic #{&1}."
+      })
+    )
+  end
+end
+
+KYC.DataSeeder.seed()
